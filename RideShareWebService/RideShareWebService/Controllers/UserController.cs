25fc4c7c5d93454130f488158a7ed4b9ccbf388a
himalie1 <himalie1@gmail.com>
2014-11-08 11:47:37 +0000

    himalie: Uploading the initial project

---
+++ RideShareWebService/RideShareWebService/Controllers/UserController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using RideShareWebService.BL;
+using RideShareWebService.Models;
+
+namespace RideShareWebService.Controllers
+{
+    public class UserController : ApiController
+    {
+        private UserBL user_bl;
+        public UserController()
+        {
+            this.user_bl = new UserBL();
+        }
+
+        // GET api/user/5
+        public List<User> Get()
+        {
+            return user_bl.GetAllUsers();
+        }
+
+        public HttpResponseMessage Post(User user)
+        {
+            this.user_bl.RegisterUser(user);
+
+            var response = Request.CreateResponse<User>(System.Net.HttpStatusCode.Created, user);
+
+            return response;
+        }
+
+
+
+
+
+        //// GET api/user
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        // POST api/user
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/user/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/user/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ RideShareWebService/RideShareWebService/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RideShareWebService.WebApiApplication" Language="C#" %>
