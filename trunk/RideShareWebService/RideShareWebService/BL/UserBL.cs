2b687326c0571bc721d9ad251ad511ab6b38a230
himalie1@gmail.com <himalie1@gmail.com@e9e0c96e-ad35-6b01-30ee-84fc1e7b3f80>
2014-11-08 11:47:37 +0000

    himalie: Uploading the initial project

---
+++ trunk/RideShareWebService/RideShareWebService/BL/UserBL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RideShareWebService.DAL;
+using RideShareWebService.Models;
+
+namespace RideShareWebService.BL
+{
+    public class UserBL
+    {
+        UserRepository user_repository = new UserRepository();
+
+        public List<User> GetAllUsers()
+        {
+            return user_repository.GetAllUsers();
+        }
+
+        public bool RegisterUser(User user)
+        {
+            return user_repository.SaveUser(user);
+        }
+    }
+}
---
+++ trunk/RideShareWebService/RideShareWebService/DAL/UserRepository.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RideShareWebService.Models;
+
+
+namespace RideShareWebService.DAL
+{
+    public class UserRepository
+    {
+        private const string CacheKey = "UserStore";
+        RideShareEntities db = new RideShareEntities();
+        
+        public UserRepository()
+        {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                if (ctx.Cache[CacheKey] == null)
+                {
+                    var users = db.Users.ToList();                    
+                    ctx.Cache[CacheKey] = users;
+                }
+            }
+        }
+
+        public List<User> GetAllUsers()
+        {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                // if cache is not null return from cache
+                return (List<User>)ctx.Cache[CacheKey];
+            }
+
+            // if not data in cache return from database
+            return db.Users.ToList();
+        }
+
+        public bool SaveUser(User user)
+        {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                try
+                {
+                    // add information to the cache
+                    var currentData = ((User[])ctx.Cache[CacheKey]).ToList();
+                    currentData.Add(user);
+                    ctx.Cache[CacheKey] = currentData.ToArray();
+
+                    // add data to the database
+                    db.Users.Add(user);
+                    db.SaveChanges();
+
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    return false;
+                }
+            }
+
+            return false;
+        }
+    }
+}
